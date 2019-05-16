ff4c69a7d5171ce11e0e102f7e9cbf60896a01cf
Ryan Block <rblock@gmail.com>
2019-05-16 14:03:59 -0700

    Integrate new endpoints

--- src/http/get-index/index.js
+++ src/http/get-index/index.js
@@ -12,10 +12,13 @@ exports.handler = async function http() {
   }
 
   let cwd = process.cwd()
-  let url = process.env.ENDPOINT + '/commands'
-  let result
+  let AWSLinux2URL = process.env.ENDPOINT + '/aws-linux-2'
+  let AMI201803URL = process.env.ENDPOINT + '/ami-2018-03'
+  let AWSLinux2
+  let AMI201803
   try {
-    result = await tiny.get({url})
+    AWSLinux2 = await tiny.get({url: AWSLinux2URL})
+    AMI201803 = await tiny.get({url: AMI201803URL})
   }
   catch (e) {
     console.log(e)
@@ -27,10 +30,15 @@ exports.handler = async function http() {
   }
 
   // Prep results
-  let results = Array.from(new Set(result.body))
-  results.shift()
-  results.map((cmd,i) => {
-    results[i] = '- `' + cmd + '`'
+  let AWSLinux2Results = Array.from(new Set(AWSLinux2.body))
+  AWSLinux2Results.shift()
+  AWSLinux2Results.map((cmd,i) => {
+    AWSLinux2Results[i] = '- `' + cmd + '`'
+  })
+  let AMI201803Results = Array.from(new Set(AMI201803.body))
+  AMI201803Results.shift()
+  AMI201803Results.map((cmd,i) => {
+    AMI201803Results[i] = '- `' + cmd + '`'
   })
 
   // Intended for local dev!
@@ -38,7 +46,8 @@ exports.handler = async function http() {
   let readme = fs.readFileSync(template).toString()
   let date = new Date(Date.now()).toISOString()
   let dated = readme.replace('LAST_UPDATED', date)
-  let final = dated.replace('SHELL_COMMANDS', results.join('\n'))
+  let final = dated.replace('$AWS_LINUX_2_SHELL_COMMANDS', AWSLinux2Results.join('\n'))
+                   .replace('$AMI_2018_03_SHELL_COMMANDS', AMI201803Results.join('\n'))
   fs.writeFileSync(path.join(cwd, '..', '..', '..', 'readme.md'), final)
   return {
     type: 'text/html; charset=utf8',
